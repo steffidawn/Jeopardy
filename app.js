284c6005bdf5c4af8a61464835f413ccef1a89d3
hsg2007@yahoo.de <YOUR-EMAIL-ADDRESS>
2017-09-18 10:41:32 -0700

    current working version 9/18

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
 var categories = [];
 var clues = [];
 var properCategories = [];
-var seanArr = ["You think you're pretty smart, don't you, Trebek? What with your Diego mustache and your greasy hair!", "Moo", "Febtober", "Potent Potables!"];
+var seanArr = ["You think you're pretty smart, don't you, Trebek? What with your Diego mustache and your greasy hair!", "Moo", "Febtober", "Potent Potables!", "Colors that end in -urple"];
 var player = "";
 var sean = "Sean";
 var ken = "Ken";
@@ -12,6 +12,10 @@ var seanScore = 0;
 var kenScore = 0;
 var humanScore = 0;
 var timer;
+var clickedSquare;
+var turns = 0;
+
+
 
 
 
@@ -37,7 +41,12 @@ $.get('http://jservice.io/api/categories?count=10', {
 			clues[i] = foo;
 		});
 	  } 
-	});
+});
+
+
+$('#instructions').click(function(){
+	$('#welcomeModal').show();
+});
 
 function showCategories(){
 	$('#category1').append('<p>' + properCategories[0].title + '</p>');
@@ -46,182 +55,217 @@ function showCategories(){
 	$('#category4').append('<p>' + properCategories[3].title + '</p>');
 	$('#category5').append('<p>' + properCategories[4].title + '</p>');
 	$('#category6').append('<p>' + properCategories[5].title + '</p>');
-}
+};
 
-$('#submitName').click(function(){
-	//add append name to textboxes
+$('.start').click(function(){
 	$('#welcomeScreen').hide();
 	$('#gameBoard').show();
 	showCategories();
 });
 
+
 $('.squares').click(function(e){
-	// console.log(e.target.id);
-	var squareID = e.target.id;
-	var squareArr = squareID.split('-');
-	console.log(squareArr);
-	// console.log(clues[0][0].question);
+	turns++
+	console.log(turns);
+	clickedSquare = $('#' + e.target.id);
+	var squareArr = e.target.id.split('-');
 	$('#gameBoard').hide();
 	$('#clueScreen').show();
-	$(this).off('click');
-	$(this).remove('p')
-	$('.clue').append('<p>' + clues[ squareArr[0] ][ squareArr[1] ].question + '</p>')
-	$('#answerCheck').append('<p id="hiddenAnswer">' + clues[ squareArr[0] ][ squareArr[1] ].answer + '</p>');
+	$('.clue').append('<p>' + clues[ squareArr[0] ][ squareArr[1] ].question + '</p>');
+	$('.clue').append("<p id='hiddenAnswer'>" + clues[ squareArr[0] ][ squareArr[1] ].answer + '</p>');
 	buzzerTimer();
-	$('#submitAnswer').click(function(e){
-	updateScore();
-	})
-	
 });
 
-$('#buzzer').click(function() {
-	console.log('clicked the buzzer')
-	$('.response').show();
-	clearTimeout(timer);
-}); 
+
+
 
 function buzzerTimer () {
 	timer = setTimeout(function(timer) {
 		$('#buzzer').off('click');
 		aiPlayer();
 	 }, 5000);
-}
+};
 
 
+$('#buzzer').click(function() {
+	$('.response').show();
+	clearTimeout(timer);
+}); 
 
-$('#answerCheck').append($('#hiddenAnswer').val());
+
+$('#submitAnswer').click(function(){
+	updateScore();
+});
 
 
-function aiPlayer() {
-	var num = Math.random;
-	if (num < 0.5) {
-		// player = "ken";
-		$('#answerCheck').append($('#hiddenAnswer').val());
-		kenUpdate();
+function nextTurn() {
+	if (turns === 3){
+		endGame();
 	}else{
-		// AI = "sean";
-		$('#answerCheck').append(seanArr[Math.floor(Math.random()*seanArr.length)]);
-		seanUpdate();
+		$('.clue').html('');
+		$('#answerCheck').html('');
+		$('#trebekReaction').html('');
+		$('#answerInput').val('');
+		$('.response').hide();
 	}
-}
+};
 
 
-function updateScore() {
-	if ($('#answerInput').val() === $('#hiddenAnswer').val()) {
-		$('#hiddenAnswer').show();
-		addScore();
-	}else{
- 		$('#hiddenAnswer').show();
-	}	subtractScore();
-}
 
-function addScore () {
-	$('.squares').click(function() {
-    switch (true) {
-      case $(this).hasClass('200clues'):
-        humanScore + 200;
-        break;
-      case $(this).hasClass('400clues'):
-        humanScore + 400;
-        break;
-      case $(this).hasClass('600clues'):
-        humanScore + 600;
-        break;
-      case $(this).hasClass('800clues'):
-        humanScore + 800;
-        break;
-      case $(this).hasClass('1000clues'):
-        humanScore + 1000;
-        break;
-    }
-}
-)};
+function addScore() {
+	if (clickedSquare.parent().hasClass('200clues')){
+		humanScore = humanScore + 200;
+		$('#humanDollars').html('$ ' + humanScore);
+		$('#trebekReaction').append("<img id='correctTrebekGif' src='correctTrebek.gif'>");
+	}else if (clickedSquare.parent().hasClass('400clues')){
+		humanScore = humanScore + 400;
+		$('#humanDollars').html('$ ' + humanScore);
+		$('#trebekReaction').append("<img id='correctTrebekGif' src='correctTrebek.gif'>");
+	}else if (clickedSquare.parent().hasClass('600clues')) {
+		humanScore = humanScore + 600;
+    	$('#humanDollars').html('$ ' + humanScore);
+    	$('#trebekReaction').append("<img id='correctTrebekGif' src='correctTrebek.gif'>");
+	}else if (clickedSquare.parent().hasClass('800clues')) {
+		humanScore = humanScore + 800;
+        $('#humanDollars').html('$ ' + humanScore);
+        $('#trebekReaction').append("<img id='correctTrebekGif' src='correctTrebek.gif'>");
+	}else if (clickedSquare.parent().hasClass('1000clues')) {
+		humanScore = humanScore + 1000;
+		$('#humanDollars').html('$ ' + humanScore);
+		$('#trebekReaction').append("<img id='correctTrebekGif' src='correctTrebek.gif'>");
+	}
+};
 
 
 function subtractScore () {
-	$('.squares').click(function() {
-    switch (true) {
-      case $(this).hasClass('200clues'):
-        humanScore - 200;
-        break;
-      case $(this).hasClass('400clues'):
-        humanScore - 400;
-        break;
-      case $(this).hasClass('600clues'):
-        humanScore - 600;
-        break;
-      case $(this).hasClass('800clues'):
-        humanScore - 800;
-        break;
-      case $(this).hasClass('1000clues'):
-        humanScore - 1000;
-        break;
-    }
-}
-)};
+    if (clickedSquare.parent().hasClass('200clues')){
+		humanScore = humanScore - 200;
+		$('#humanDollars').html('$ ' + humanScore);
+		$('#trebekReaction').append("<img id='wrongTrebekGif' src='wrongTrebek.gif'>");
+	}else if (clickedSquare.parent().hasClass('400clues')){
+		humanScore = humanScore - 400;
+		$('#humanDollars').html('$ ' + humanScore);
+		$('#trebekReaction').append("<img id='wrongTrebekGif' src='wrongTrebek.gif'>");
+	}else if (clickedSquare.parent().hasClass('600clues')) {
+		humanScore = humanScore - 600;
+    	$('#humanDollars').html('$ ' + humanScore);
+    	$('#trebekReaction').append("<img id='wrongTrebekGif' src='wrongTrebek.gif'>");
+	}else if (clickedSquare.parent().hasClass('800clues')) {
+		humanScore = humanScore - 800;
+        $('#humanDollars').html('$ ' + humanScore);
+        $('#trebekReaction').append("<img id='wrongTrebekGif' src='wrongTrebek.gif'>");
+	}else if (clickedSquare.parent().hasClass('1000clues')) {
+		humanScore = humanScore - 1000;
+		$('#humanDollars').html('$ ' + humanScore);
+		$('#trebekReaction').append("<img id='wrongTrebekGif' src='wrongTrebek.gif'>");
+	}
+};
+
+
+function updateScore() {
+	if ($('#answerInput').val() === $('#hiddenAnswer').text()) {
+		$('#answerCheck').append($('#answerInput').val());
+		addScore();
+	}else{
+		$('#answerCheck').append($('#answerInput').val());
+		subtractScore();
+	}
+};
+
+
 
 function kenUpdate () {
-	$('.squares').click(function() {
-	    switch (true) {
-	      case $(this).hasClass('200clues'):
-	        kenScore + 200;
-	        break;
-	      case $(this).hasClass('400clues'):
-	        kenScore + 400;
-	        break;
-	      case $(this).hasClass('600clues'):
-	        kenScore + 600;
-	        break;
-	      case $(this).hasClass('800clues'):
-	        kenScore + 800;
-	        break;
-	      case $(this).hasClass('1000clues'):
-	        kenScore + 1000;
-	        break;
-	    }
+	if (clickedSquare.parent().hasClass('200clues')){
+		kenScore = kenScore + 200;
+		$('#kenDollars').html('$ ' + kenScore);
+		$('#trebekReaction').append("<img id='kenGif' src='croppedKenGif.gif'>");
+	}else if (clickedSquare.parent().hasClass('400clues')){
+		kenScore = kenScore + 400;
+		$('#kenDollars').html('$ ' + kenScore);
+		$('#trebekReaction').append("<img id='kenGif' src='croppedKenGif.gif'>");
+	}else if (clickedSquare.parent().hasClass('600clues')) {
+		kenScore = kenScore + 600;
+    	$('#kenDollars').html('$ ' + kenScore);
+    	$('#trebekReaction').append("<img id='kenGif' src='croppedKenGif.gif'>");
+	}else if (clickedSquare.parent().hasClass('800clues')) {
+		kenScore = kenScore + 800;
+        $('#kenDollars').html('$ ' + kenScore);
+        $('#trebekReaction').append("<img id='kenGif' src='croppedKenGif.gif'>");
+	}else if (clickedSquare.parent().hasClass('1000clues')) {
+		kenScore = kenScore + 1000;
+		$('#kenDollars').html('$ ' + kenScore);
+		$('#trebekReaction').append("<img id='kenGif' src='croppedKenGif.gif'>");
 	}
-)};
+};
 
 
 function seanUpdate() {
-	$('.squares').click(function() {
-	    switch (true) {
-	      case $(this).hasClass('200clues'):
-	        seanScore - 200;
-	        break;
-	      case $(this).hasClass('400clues'):
-	        seanScore - 400;
-	        break;
-	      case $(this).hasClass('600clues'):
-	        seanScore - 600;
-	        break;
-	      case $(this).hasClass('800clues'):
-	        seanScore - 800;
-	        break;
-	      case $(this).hasClass('1000clues'):
-	        seanScore - 1000;
-	        break;
-	    }
+	if (clickedSquare.parent().hasClass('200clues')){
+		seanScore = seanScore - 200;
+		$('#seanDollars').html('$ ' + seanScore);
+		$('#trebekReaction').append("<img id='seanGif' src='seanReaction.gif'>");
+	}else if (clickedSquare.parent().hasClass('400clues')){
+		seanScore = seanScore - 400;
+		$('#seanDollars').html('$ ' + seanScore);
+		$('#trebekReaction').append("<img id='seanGif' src='seanReaction.gif'>");
+	}else if (clickedSquare.parent().hasClass('600clues')) {
+		seanScore = seanScore - 600;
+    	$('#seanDollars').html('$ ' + seanScore);
+    	$('#trebekReaction').append("<img id='seanGif' src='seanReaction.gif'>");
+	}else if (clickedSquare.parent().hasClass('800clues')) {
+		seanScore = seanScore - 800;
+        $('#seanDollars').html('$ ' + seanScore);
+        $('#trebekReaction').append("<img id='seanGif' src='seanReaction.gif'>");
+	}else if (clickedSquare.parent().hasClass('1000clues')) {
+		seanScore = seanScore - 1000;
+		$('#seanDollars').html('$ ' + seanScore);
+		$('#trebekReaction').append("<img id='seanGif' src='seanReaction.gif'>");
 	}
-)};
+};
+
+function aiPlayer() {
+	var num = Math.random();
+	if (num < 0.5) {
+		var hiddenAnswer = $('#hiddenAnswer').html();
+		console.log(hiddenAnswer);
+		$('#answerCheck').html(hiddenAnswer);
+		kenUpdate();
+	}else{
+		$('#answerCheck').html(seanArr[Math.floor(Math.random()*seanArr.length)]);
+		seanUpdate();
+	}
+};
+
+$('#back').click(function(){
+	$('#clueScreen').hide();
+	$('#gameBoard').show();
+	clickedSquare.off('click');
+	clickedSquare.html('');
+	nextTurn();
+});
 
 
+function endGame(){
+	if (humanScore > kenScore && humanScore > seanScore) {
+		alert("You won!");
+		location.reload();
+		// $('#resultsModal').html('<p>You won!</p>');
+		// $('#resultsModal').show();
+	}else if (kenScore > humanScore && kenScore > seanScore){
+		alert("Ken won!");
+		location.reload();
+		// $('#resultsModal').html('<p>Ken Jennings!</p>');
+		// $('#resultsModal').show();
+	}else if (seanScore > humanScore && seanScore > kenScore){
+		alert("Sean won!");
+		location.reload();
+		// $('#resultsModal').html('<p>SeanConnery!</p>');
+		// $('#resultsModal').show();
+	}
+	// $('#playAgain').click(function(){
+	// 	location.reload();
+	// })
+};
 
 
-//Welcome Screen
-//#Input field for name that will fill in name in name text box in other screens
-//#Click on submit name button will trigger the #gameBoard to show
-
-//Game Board
-//#
-//#Click events for each square that randomly pull data out of api. 
-	//#Human has XXX seconds to hit buzzer (space bar?), otherwise one of the to AIs will be randomly chosen. 
-	//#Sean Connery will pick an answer out of a stupid answer array (always wrong) and Ken Jennings will pick out of a correct answer array (always right).
-//#Clue would be displayed in clue screen, answer to be hidden until player has submitted an answer.
-//#Text in square div emptied once a that square has been selected
-
-
-//clueScreen
-	//#Random clue from API will create a new <p> into the clue div displayed on the clueScreen.
-	//#Answer input by human player will be compared to answer in API
 });
